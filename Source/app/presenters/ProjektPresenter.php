0a5c364e0d25da83992a9885a93492331b67673d
Roman Mátyus <romanmatyus@gmail.com>
2014-03-24 09:34:07 +0100

    Merge KonkretnyProjekt and Projekt

--- Source/app/presenters/ProjektPresenter.php
+++ Source/app/presenters/ProjektPresenter.php
@@ -13,6 +13,8 @@ use Nette,
 
 class ProjektPresenter extends BasePresenter
 {
+	public $id;
+
         public function handleGetIn($projectID)
         {      
             $user = $this->getUser();
@@ -26,25 +28,12 @@ class ProjektPresenter extends BasePresenter
                     );
                     $project_member->insert($data);
 
-                    $projekt_S = $this->getSession('projekt_S');
-                    $projekt_S->setExpiration(0);
-                    $projekt_S->pr_id = $projectID;
-
                     $this->flashMessage("Pridaný do projektu.");
-                    $this->redirect('KonkretnyProjekt:default');
+                    $this->redirect('detail', $projectID);
                 }else{
                     $this->flashMessage("Nemôžeš byť pridaný, lebo si zakladateľom tohto projektu!");
                 }
             }
-        }
-        
-        public function handleKProjekt($projectID)
-        {            
-            $projekt_S = $this->getSession('projekt_S');
-            $projekt_S->setExpiration(0);
-            $projekt_S->pr_id = $projectID;
-            
-            $this->redirect('KonkretnyProjekt:default');
         }
     
 	public function renderDefault()
@@ -88,6 +77,56 @@ class ProjektPresenter extends BasePresenter
                 $this->flashMessage("Nikto nie je prihlásený.");
             }
         }
+
+	public function renderDetail($id)
+	{
+		$this->id = $id;
+		$user = $this->getUser();
+		$this->template->user = $user;
+
+		$this->template->konkretny_projekt = $this->database->table("project")->where("id",$id)->fetch();   
+
+		$owner = $this->database->table("project")->where("id",$id)->fetch()->owner;
+		$admin = $this->database->table("users")->where("id",$owner)->fetch()->name;
+		$this->template->admin = $admin;
+
+		$this->template->members = $this->database->table("project_member")->where("project_id",$id);
+		$this->template->users = $this->database->table("users");
+	}
+
+	public function createComponentListTasks()
+	{
+		$konkretny_projekt = $this->database->table("project")->where("id",$this->id)->fetch(); 
+		$form = new Form;
+		$form->addText("name","Názov úlohy")
+			->setRequired("Zadaj meno úlohy!")
+			->getControlPrototype()->setClass("form-control");
+		$form->addtextArea("description","Popis úlohy",NULL,8)
+			->setRequired("Zadaj popis úlohy!")
+			->getControlPrototype()->setClass("form-control");
+		$items = array(
+			$konkretny_projekt->owner => $konkretny_projekt->ref("users","owner")->name,
+		);
+		foreach($konkretny_projekt->related("project_member") as $user){
+			$items[$user->user->id] = $user->user->name;
+		}
+		$form->addSelect("worker","Členovia",$items)
+			->getControlPrototype()->setClass("form-control");
+		$form->addSubmit("send","Pridať úlohu")
+			->getControlPrototype()->setClass("btn-u");
+		$form->onSuccess[] = $this->listTasksSucceeded;
+		return $form;
+	}
+
+	public function listTasksSucceeded($form)
+	{
+		$values = $form->getValues();
+		$values['owner'] = $this->user->id;
+		$values['project_id'] = $this->id;
+		$this->database->table("task")->insert($values); 
+		$this->flashMessage("Úloha pridaná!");
+		$this->redirect('detail',$this->id);
+	}
 }
 /*
             $data = array(
