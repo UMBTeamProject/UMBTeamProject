9ec3e6aec25da2a7a6d97e079a63bd5d2f1122a3
Roman Mátyus <romanmatyus@gmail.com>
2014-03-24 14:33:19 +0100

    Add support for task and comments

--- Source/app/presenters/TaskPresenter.php
+++ Source/app/presenters/TaskPresenter.php
@@ -13,7 +13,45 @@ use Nette,
 
 class TaskPresenter extends BasePresenter
 {
+	/** @persistent */
+	public $id;
+
 	public function renderDetail($id)
 	{
+		$this->id = $id;
+		$this->template->task = $this->database->table("task")->where("id",$id)->fetch();
+	}
+
+	public function createComponentAddComment()
+	{
+		$form = new Form;
+		$form->addTextarea("text","Komentár:")
+			->setAttribute("rows",8)
+			->getControlPrototype()
+			->setClass("form-control");
+		$form->addSubmit("send","Poslať komentár")
+			->getControlPrototype()
+			->setClass("btn-u");
+		$form->onSuccess[] = $this->addComment;
+		return $form;
+	}
+
+	public function addComment($form)
+	{
+		if (!$this->user->isLoggedIn()) {
+			$this->flashMessage("Pridávanie komentárov je povolené iba pre prihlásených.", "warning");
+		} else {
+		$this->database->table('comment')
+			->insert(
+				array_merge(
+					(array) $form->getValues(),
+					array(
+						"task" => $this->id,
+						"author" => $this->user->id,
+					)
+				)
+			);
+		}
+		$this->redirect('this');
 	}
 }
